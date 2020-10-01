b88af2c6ac241cf303f8c164e41cca2d2bfbe011
naJ123456789 <72186086+naJ123456789@users.noreply.github.com>
2020-10-01 11:50:55 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -3,7 +3,7 @@ int main() {
 
     int number1, number2, sum;
     
-    printf("Enter two integers: ");
+    printf("Enter two integers to add: ");
     scanf("%d %d", &number1, &number2);
 
     // calculating sum
