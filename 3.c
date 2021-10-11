7fa787a58be73adc61c4d316d249a2b3c50b751c
NAJEEB <60335647+ceccs18c41@users.noreply.github.com>
2021-10-11 22:55:37 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -12,3 +12,16 @@ int main() {
     printf("%d + %d = %d", number1, number2, sum);
     return 0;
 }
+void function fizzbuzz {
+  for (i = 1; i <= 100; i++) {
+    set print_number to true;
+    If i is divisible by 3 {
+      print "Fizz";
+      set print_number to false; }
+    If i is divisible by 5 {
+      print "Buzz";
+      set print_number to false; }
+    If print_number, print i;
+    print a newline;
+  }
+}
