263fcc9564c723ebf22158450c0500e5c12f73af
naJ123456789 <72186086+naJ123456789@users.noreply.github.com>
2021-10-12 22:57:57 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -12,16 +12,4 @@ int main() {
     printf("%d + %d = %d", number1, number2, sum);
     return 0;
 }
-void function fizzbuzz {
-  for (i = 1; i <= 100; i++) {
-    set print_number to true;
-    If i is divisible by 3 {
-      print "Fizz";
-      set print_number to false; }
-    If i is divisible by 5 {
-      print "Buzz";
-      set print_number to false; }
-    If print_number, print i;
-    print a newline;
-  }
-}
+
